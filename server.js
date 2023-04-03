55381aef3bec352f5909049c4312027f8788a08d
YousifMHelal <yousifm.helal@gmail.com>
2023-04-03 20:30:49 +0200

    Update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: 'postgres://smartbraindb_tyjw_user:fjHi3JQSzGucGVyPlKB5Po7rHQYUSlsv@dpg-cgjia8u4dadak461gbug-a/smartbraindb_tyjw',
-        ssl: {rejeectUnauthorized: false},
+        ssl: { rejeectUnauthorized: false },
         host: 'dpg-cgjia8u4dadak461gbug-a',
         port: 5432,
         user: 'smartbraindb_tyjw_user',
@@ -27,39 +27,11 @@ app.get('/', (req, res) => {
 
 //post user sign in
 app.post('/signin', (req, res) => {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-        return res.status(400).json('incorrect form submission');
-    }
-
-    db.select('email', 'hash').from('login')
-        .where('email', '=', email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
-            }
-            else {
-                res.status(400).json('wrong')
-            }
-        })
-        .catch(err => res.status(400).json('wrong2'))
-})
-
-//post user register
-app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-
+    
     if (!email || !name || !password) {
         return res.status(400).json('incorrect form submission');
     }
-
     const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
         trx.insert({
@@ -84,21 +56,6 @@ app.post('/register', (req, res) => {
             .catch(trx.rollback)
     })
         .catch(err => res.status(400).json('unable to register'))
-
-})
-
-//get user profile
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({ id: id })
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('user not found')
-            }
-        })
-        .catch(err => res.status(400).json('error getting user'))
 })
 
 //update entries count
